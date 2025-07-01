ac908910748acb4e6095c4d57453adce3deca6b1
Roxanne-Hudan <roxanne.colosia@hotmail.com>
2025-06-30 19:00:25 -0600

    adicion de boton de busqueda en post

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.db.models import Q
 #create
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
@@ -22,6 +23,15 @@ class PostListView(ListView):
     model = Post
     template_name = 'AppCoder/Blog/post_list.html'
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(titulo__icontains=query) | Q(subtitulo__icontains=query)
+            )
+        return queryset
 
 class PostDetailView(DetailView):
     model = Post
